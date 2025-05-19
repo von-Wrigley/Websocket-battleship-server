f0590560f23be1dde307cda42a19e8461ad724dc
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-05-19 19:58:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { httpServer } from "./src/http_server/index.js";
+import {startwsServer} from "./src/http_server/server.js";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT =  process.env.HTTP_PORT || 8181;
+const port = process.env.PORT || 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+httpServer.listen(+HTTP_PORT);
+startwsServer(+port)
